cc36092f0ce7901c475b4c4bf8408961c358ed15
Moritz Lenz <moritz@faui2k3.org>
2011-08-31 16:48:00 +0200

    text renderer (untested)

---
+++ lib/Chalice/Model/Renderer/Text.pm
@@ -0,0 +1,18 @@
+use 5.010;
+package Chalice::Model::Renderer::Text;
+
+sub render_cost { 1 }
+sub render {
+    my $text = $_[1];
+    my @paragraphs = split /\n\s*\n/, $text;
+    my %html_escapes = (
+        '&' => '&amp;',
+        '<' => '&lt;',
+        '>' => '&gt;',
+        '"' => '&quot;',
+    );
+    for (@paragraphs) {
+        s/([&<>"])/$html_escapes{$1}/g;
+    }
+    return join "\n", map "<p>$_</p>\n", @paragraphs;
+}
